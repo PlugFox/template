be1f47d5dbe20f1082ef6e809db152ee626c4a0f
Plague Fox <plugfox@gmail.com>
2024-01-03 01:44:38 +0400

    Update rename script

--- tool/dart/rename_project.dart
+++ tool/dart/rename_project.dart
@@ -6,6 +6,23 @@ import 'package:path/path.dart' as p;
 const String _defaultName = 'flutter_template_name';
 const String _defaultOrganization = 'dev.flutter.template';
 const String _defaultDescription = 'flutter_template_description';
+const Set<String> _extensions = {
+  '.dart',
+  '.yaml',
+  '.gradle',
+  '.xml',
+  '.kt',
+  '.plist',
+  '.txt',
+  '.cc',
+  '.cpp',
+  '.rc',
+  '.xcconfig',
+  '.pbxproj',
+  '.xcscheme',
+  '.html',
+  '.json',
+};
 
 /// dart run tool/dart/rename_project.dart --name="qqq" --organization="www" --description="eee"
 void main([List<String>? args]) {
@@ -20,15 +37,15 @@ void main([List<String>? args]) {
         .trim();
   }
 
-  final n = extractArg('--name');
-  final o = extractArg('--organization');
-  final d = extractArg('--description');
-  if (n == null || o == null || d == null) _throwArguments();
-  _renameDirectory(_defaultName, n);
+  final name = extractArg('--name');
+  final org = extractArg('--organization');
+  final desc = extractArg('--description');
+  if (name == null || org == null || desc == null) _throwArguments();
+  _renameDirectory(_defaultName, name);
   _changeContent([
-    (from: _defaultName, to: n),
-    (from: _defaultOrganization, to: o),
-    (from: _defaultDescription, to: d),
+    (from: _defaultName, to: name),
+    (from: _defaultOrganization, to: org),
+    (from: _defaultDescription, to: desc),
   ]);
 }
 
@@ -40,15 +57,29 @@ Never _throwArguments() {
   io.exit(1);
 }
 
-void _renameDirectory(String from, String to) => io.Directory(from)
-    .listSync(recursive: true)
-    .whereType<io.Directory>()
-    .where((dir) => p.basename(dir.path) == from)
-    .forEach((dir) => dir.renameSync(p.join(p.dirname(dir.path), to)));
+Iterable<io.FileSystemEntity> _recursiveDirectories(
+    io.Directory directory) sync* {
+  for (final e in directory.listSync(recursive: false, followLinks: false)) {
+    if (e is io.File) {
+      if (_extensions.contains(p.extension(e.path))) continue;
+      yield e;
+    } else if (e is io.Directory) {
+      if (p.basename(e.path).startsWith('.')) continue;
+      yield e;
+      yield* _recursiveDirectories(e);
+    }
+  }
+}
+
+void _renameDirectory(String from, String to) =>
+    _recursiveDirectories(io.Directory.current)
+        .whereType<io.Directory>()
+        .toList(growable: false)
+        .where((dir) => p.basename(dir.path) == from)
+        .forEach((dir) => dir.renameSync(p.join(p.dirname(dir.path), to)));
 
 void _changeContent(List<({String from, String to})> pairs) =>
-    io.Directory.current
-        .listSync(recursive: true)
+    _recursiveDirectories(io.Directory.current)
         .whereType<io.File>()
         .forEach((e) {
       var content = e.readAsStringSync();
