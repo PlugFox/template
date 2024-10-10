11288994474c3f9af2ec4674e43b5a178aae0e14
Plague Fox <plugfox@gmail.com>
2024-10-10 20:11:13 +0400

    Add tests example

--- lib/src/feature/authentication/data/authentication_repository.dart
+++ lib/src/feature/authentication/data/authentication_repository.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
 
+import 'package:flutter/widgets.dart';
 import 'package:flutter_template_name/src/feature/authentication/model/sign_in_data.dart';
 import 'package:flutter_template_name/src/feature/authentication/model/user.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -58,6 +59,57 @@ class AuthenticationRepositoryImpl implements IAuthenticationRepository {
         () {
           const user = User.unauthenticated();
           _sharedPreferences.remove(_sessionKey).ignore();
+          _userController.add(_user = user);
+        },
+      );
+}
+
+@visibleForTesting
+class AuthenticationRepositoryFake implements IAuthenticationRepository {
+  AuthenticationRepositoryFake({
+    Map<String, Object?>? store,
+  }) : _store = store ?? <String, Object?>{};
+
+  static const String _sessionKey = 'authentication.session';
+  final Map<String, Object?> _store;
+  final StreamController<User> _userController = StreamController<User>.broadcast();
+  User _user = const User.unauthenticated();
+
+  @override
+  FutureOr<User> getUser() => _user;
+
+  @override
+  Stream<User> userChanges() => _userController.stream;
+
+  @override
+  Future<User> signIn(SignInData data) => Future<User>.delayed(
+        const Duration(seconds: 1),
+        () {
+          final user = User.authenticated(id: data.username);
+          _store[_sessionKey] = jsonEncode(user.toJson());
+          _userController.add(_user = user);
+          return user;
+        },
+      );
+
+  @override
+  Future<User?> restore() async {
+    final session = _store[_sessionKey];
+    if (session is! String) return null;
+    final json = jsonDecode(session);
+    if (json case Map<String, Object?> jsonMap) {
+      final user = User.fromJson(jsonMap);
+      _userController.add(_user = user);
+    }
+    return _user;
+  }
+
+  @override
+  Future<void> signOut() => Future<void>.delayed(
+        Duration.zero,
+        () {
+          const user = User.unauthenticated();
+          _store.remove(_sessionKey);
           _userController.add(_user = user);
         },
       );
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,12 +1,48 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_template_name/src/common/model/dependencies.dart';
+import 'package:flutter_template_name/src/common/widget/app.dart';
+import 'package:flutter_template_name/src/feature/authentication/controller/authentication_controller.dart';
+import 'package:flutter_template_name/src/feature/authentication/data/authentication_repository.dart';
+import 'package:flutter_template_name/src/feature/settings/widget/settings_scope.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:octopus/octopus.dart';
 
 void main() => group('Widget', () {
       testWidgets(
-        'Placeholder',
+        'Dependencies_are_injected',
         (tester) async {
-          await tester.pumpWidget(const SizedBox());
-          expect(find.byType(SizedBox), findsOneWidget);
+          await tester.pumpWidget(FakeDependencies().inject(child: Container()));
+          expect(find.byType(Container), findsOneWidget);
+          expect(find.byType(InheritedDependencies), findsOneWidget);
+          final context = tester.element(find.byType(Container));
+          expect(
+            Dependencies.of(context),
+            allOf(
+              isNotNull,
+              isA<Dependencies>(),
+              isA<FakeDependencies>(),
+            ),
+          );
+        },
+      );
+
+      testWidgets(
+        'App',
+        (tester) async {
+          final dependencies = FakeDependencies()
+            ..authenticationController = AuthenticationController(
+              repository: AuthenticationRepositoryFake(),
+            );
+          await tester.pumpWidget(dependencies.inject(
+            child: const SettingsScope(
+              child: NoAnimationScope(
+                noAnimation: true,
+                child: App(),
+              ),
+            ),
+          ));
+          expect(find.byType(MaterialApp), findsOneWidget);
+          expect(find.byType(InheritedDependencies), findsOneWidget);
         },
       );
     });
